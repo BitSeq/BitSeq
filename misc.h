579a60861be20843bbdd781e9b047b2e86b62a89
mbaxjpg3 <mbaxjpg3@c4856536-7db2-4565-afe3-bcf1ab8f3238>
2013-03-14 17:29:23 +0000

    Adding new log functions into misc.
    git-svn-id: svn://sisp.smith.man.ac.uk/svn/rattray/bitseq/cpp@25 c4856536-7db2-4565-afe3-bcf1ab8f3238

--- misc.h
+++ misc.h
@@ -6,13 +6,27 @@
 #include "ArgumentParser.h"
 #include "PosteriorSamples.h"
 
+namespace ns_math {
+
+// For a=log(x), b=log(y); compute log(x+y).
+double logAddExp(double a, double b);
+
+// For vals_i = log(x_i); compute log(sum(x_i)).
+double logSumExp(const vector<double> &vals);
+
+}
+
 namespace ns_misc {
 
+// Value to use instead of log(0).
 const double LOG_ZERO=-1000;
 
+// Return seed; either using seed set in args, or by using time(NULL) as seed.
 long getSeed(const ArgumentParser &args);
 
+// Open output file based on standard argument --outFile=<outFileName>.
 bool openOutput(const ArgumentParser &args, ofstream *outF);
+// Open output file of a give name.
 bool openOutput(const string &name, ofstream *outF);
 
 // Reads and initializes files containing samples fro each condition and each replicate.
