84ea80d989ab9060ae4be0b6facdd8b453c0f596
Antti Honkela <antti.honkela@hiit.fi>
2015-05-28 14:45:15 +0300

    fix a bug with noise transcript probabilities

--- LikelihoodWriter.cpp
+++ LikelihoodWriter.cpp
@@ -59,13 +59,13 @@ void MemoryWriter::writeRead(string name, vector<ns_parseAlignment::TagAlignment
     likelihoods->alignments->pushAlignmentL(alignments[i].getTrId(),
 					    alignments[i].getProb());
   }
-  likelihoods->alignments->pushAlignment(0, minProb);
+  likelihoods->alignments->pushAlignmentL(0, minProb);
   likelihoods->alignments->pushRead();
 }
 
 void MemoryWriter::writeDummy(string name)
 {
-  likelihoods->alignments->pushAlignment(0, 0);
+  likelihoods->alignments->pushAlignmentL(0, 0);
   likelihoods->alignments->pushRead();
 }
 
